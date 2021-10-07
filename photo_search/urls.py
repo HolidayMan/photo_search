99617c50db2f789dbfab098ed2bc1804c0b98ada
Holiday Man <lesha.pitenko791@gmail.com>
2021-10-07 21:57:03 +0300

    added ability to upload files

--- photo_search/urls.py
+++ photo_search/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-from .views import IndexView, SearchView
+from .views import IndexView, SearchView, upload
 
 
 urlpatterns = [
     re_path(r'search/?', SearchView.as_view(), name='search'),
+    re_path(r'upload_file/?', upload, name='upload_file'),
     re_path(r'', IndexView.as_view(), name='index'),
 ]
--- photo_search/views.py
+++ photo_search/views.py
@@ -1,5 +1,10 @@
+import os
 from pathlib import Path
+
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+
+from photo_search_iitsyndicate.settings import BASE_DIR
 from .utils import search_for_images
 
 BASE_TEMPLATES_DIR = Path('photo_search')
@@ -24,3 +29,17 @@ class SearchView(TemplateView):
             'search_result': self.get_images()
         })
         return context
+
+
+def upload(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed", 403)
+    file = request.FILES['file']
+    if not os.path.exists(BASE_DIR / 'email_files'):
+        os.mkdir(BASE_DIR / 'email_files')
+
+    with open(BASE_DIR / 'email_files' / file.name, 'wb') as f:  # NOTE: it will rewrite existing file with the same name if one exists
+        for chunk in file.chunks():
+            f.write(chunk)
+
+    return HttpResponse("File uploaded successfully!")
